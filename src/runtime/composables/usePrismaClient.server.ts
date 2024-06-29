1d5edbf91f073a59b6db3a86238bf9e9f049d557
rrd108 <rrd@webmania.cc>
2024-06-29 18:19:25 +0200

    refactor remove prisma related code

--- src/runtime/composables/usePrismaClient.server.ts
+++
@@ -1,8 +0,0 @@
-//import { useNuxtApp } from "#imports";
-import { useNuxtApp } from "#app";
-
-export const usePrismaClient = () => {
-  const { $prisma } = useNuxtApp();
-  console.log({ $prisma });
-  return { $prisma };
-};
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,9 +1,4 @@
 import { defineNuxtPlugin } from "#app";
-//import { PrismaClient } from "@prisma/client";
-
-//export default defineNuxtPlugin((nuxtApp) => {
 export default defineNuxtPlugin(() => {
   console.log("Plugin injected by nuxt-token-authentication!");
-  //const prisma = new PrismaClient();
-  //nuxtApp.provide("prisma", prisma);
 });
